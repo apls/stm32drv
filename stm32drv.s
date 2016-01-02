9fc5a83db84f03d7ed50356bb337ec18ddfbe767
apls <bj300002@sina.com>
2016-01-02 23:04:04 +0800

    Signed-off-by: apls <bj300002@sina.com>

--- stm32drv.s
+++ stm32drv.s
@@ -111,74 +111,19 @@ __Vectors       DCD     __initial_sp               ; Top of Stack
                 DCD     0                          ; Reserved
                 DCD     PendSV_Handler             ; PendSV Handler
                 DCD     SysTick_Handler            ; SysTick Handler
-
-                ; External Interrupts
-                DCD     WWDG_IRQHandler            ; Window Watchdog
-                DCD     PVD_IRQHandler             ; PVD through EXTI Line detect
-                DCD     TAMPER_IRQHandler          ; Tamper
-                DCD     RTC_IRQHandler             ; RTC
-                DCD     FLASH_IRQHandler           ; Flash
-                DCD     RCC_IRQHandler             ; RCC
-                DCD     EXTI0_IRQHandler           ; EXTI Line 0
-                DCD     EXTI1_IRQHandler           ; EXTI Line 1
-                DCD     EXTI2_IRQHandler           ; EXTI Line 2
-                DCD     EXTI3_IRQHandler           ; EXTI Line 3
-                DCD     EXTI4_IRQHandler           ; EXTI Line 4
-                DCD     DMA1_Channel1_IRQHandler   ; DMA1 Channel 1
-                DCD     DMA1_Channel2_IRQHandler   ; DMA1 Channel 2
-                DCD     DMA1_Channel3_IRQHandler   ; DMA1 Channel 3
-                DCD     DMA1_Channel4_IRQHandler   ; DMA1 Channel 4
-                DCD     DMA1_Channel5_IRQHandler   ; DMA1 Channel 5
-                DCD     DMA1_Channel6_IRQHandler   ; DMA1 Channel 6
-                DCD     DMA1_Channel7_IRQHandler   ; DMA1 Channel 7
-                DCD     ADC1_2_IRQHandler          ; ADC1 & ADC2
-                DCD     USB_HP_CAN1_TX_IRQHandler  ; USB High Priority or CAN1 TX
-                DCD     USB_LP_CAN1_RX0_IRQHandler ; USB Low  Priority or CAN1 RX0
-                DCD     CAN1_RX1_IRQHandler        ; CAN1 RX1
-                DCD     CAN1_SCE_IRQHandler        ; CAN1 SCE
-                DCD     EXTI9_5_IRQHandler         ; EXTI Line 9..5
-                DCD     TIM1_BRK_IRQHandler        ; TIM1 Break
-                DCD     TIM1_UP_IRQHandler         ; TIM1 Update
-                DCD     TIM1_TRG_COM_IRQHandler    ; TIM1 Trigger and Commutation
-                DCD     TIM1_CC_IRQHandler         ; TIM1 Capture Compare
-                DCD     TIM2_IRQHandler            ; TIM2
-                DCD     TIM3_IRQHandler            ; TIM3
-                DCD     TIM4_IRQHandler            ; TIM4
-                DCD     I2C1_EV_IRQHandler         ; I2C1 Event
-                DCD     I2C1_ER_IRQHandler         ; I2C1 Error
-                DCD     I2C2_EV_IRQHandler         ; I2C2 Event
-                DCD     I2C2_ER_IRQHandler         ; I2C2 Error
-                DCD     SPI1_IRQHandler            ; SPI1
-                DCD     SPI2_IRQHandler            ; SPI2
-                DCD     USART1_IRQHandler          ; USART1
-                DCD     USART2_IRQHandler          ; USART2
-                DCD     USART3_IRQHandler          ; USART3
-                DCD     EXTI15_10_IRQHandler       ; EXTI Line 15..10
-                DCD     RTCAlarm_IRQHandler        ; RTC Alarm through EXTI Line
-                DCD     USBWakeUp_IRQHandler       ; USB Wakeup from suspend
-                DCD     TIM8_BRK_IRQHandler        ; TIM8 Break
-                DCD     TIM8_UP_IRQHandler         ; TIM8 Update
-                DCD     TIM8_TRG_COM_IRQHandler    ; TIM8 Trigger and Commutation
-                DCD     TIM8_CC_IRQHandler         ; TIM8 Capture Compare
-                DCD     ADC3_IRQHandler            ; ADC3
-                DCD     FSMC_IRQHandler            ; FSMC
-                DCD     SDIO_IRQHandler            ; SDIO
-                DCD     TIM5_IRQHandler            ; TIM5
-                DCD     SPI3_IRQHandler            ; SPI3
-                DCD     UART4_IRQHandler           ; UART4
-                DCD     UART5_IRQHandler           ; UART5
-                DCD     TIM6_IRQHandler            ; TIM6
-                DCD     TIM7_IRQHandler            ; TIM7
-                DCD     DMA2_Channel1_IRQHandler   ; DMA2 Channel1
-                DCD     DMA2_Channel2_IRQHandler   ; DMA2 Channel2
-                DCD     DMA2_Channel3_IRQHandler   ; DMA2 Channel3
-                DCD     DMA2_Channel4_5_IRQHandler ; DMA2 Channel4 & Channel5
 __Vectors_End
 
 
         AREA    |.text|, CODE, READONLY
         ENTRY         ;指示开始执行
-Reset_Handler
+Reset_Handler		
+	; Clear output
+	EOR R0,R0	   ;
+	LDR R1,=GPIOB
+	STR R0,[R1]		   
+	
+	;Init begin
+
     ;BL.W   RCC_CONFIG_48MHZ
 
     LDR    R1,=RCC_APB2ENR
@@ -191,7 +136,7 @@ Reset_Handler
     LDR    R1,=GPIOB_CRH ;PB.8\9 在高寄存器
     STR    R0,[R1]
     MOV    R0,#0x33333333
-    LDR    R1,=GPIOB_CRH ;PB.0-7低寄存器
+    LDR    R1,=GPIOB_CRL ;PB.0-7低寄存器
     STR    R0,[R1]
 
     LDR    R1,=GPIOB_ODR
@@ -202,11 +147,13 @@ Reset_Handler
 
 
 	MOV R6,#0
-	MOV R7, #4800 ; 10KHz 4800
+	MOV R7, #2400 ; 10KHz 2400
 	MOV R8,#0
 	MOV R9,#10
-	LDR R10,=0x123456
+	LDR R10,=0x123456 ; Output array address
 	LDR R11,=24000000
+
+	; Init finish
 
 GOON
 	; 斩波判断
@@ -217,6 +164,24 @@ GOON
 	ORR R6,R0
 	MOV R2,R5
 	EOR R2,R6	   ;
+	LDR R1,=GPIOB
+	STR R2,[R1]	 
+
+	; 自动半流判断
+	; R11 自动半流 计数
+	SUBS R11,R11,#1
+	BNE GOON
+    LDR R1,=GPIOD
+	MOV R0,#0X128
+	STR R0,[R1]
+    B GOON
+
+	; 斩波判断
+    LDR R1,=GPIOD
+    LDR R0,[R1]
+	ORR R6,R0
+	MOV R2,R5
+	EOR R2,R6
 	LDR R1,=GPIOB
 	STR R2,[R1]
 
@@ -231,7 +196,17 @@ GOON
 	TST R0, #BIT9
 	BEQ PUL_DIR
 	ADDS R9,R9,#2
-PUL_DIR
+PUL_DIR		   
+
+	; 斩波判断
+    LDR R1,=GPIOD
+    LDR R0,[R1]
+	ORR R6,R0
+	MOV R2,R5
+	EOR R2,R6
+	LDR R1,=GPIOB
+	STR R2,[R1]
+
 	SUBS R9,R9,#1
 	BNE PUL_OUT
 	MOV R9,#10
@@ -239,7 +214,7 @@ PUL_OUT
 	LDR R5,[R9,R10]	   ;
 	LDR R1,=GPIOB
 ; PWM_INIT
-	MOV R7, #4800 ; 10KHz 4800
+	MOV R7, #2400 ; 10KHz 2400
 	MOV R6,#0
 	LDR R1,=GPIOB
 	STR R5,[R1]
@@ -250,23 +225,24 @@ PUL_OUT
 	; R7 PWM 计数
 PWM_TEST
 	SUBS R7,R7,#1
-	BNE PWM_DONE
-PWM_INIT
-	MOV R7, #4800 ; 10KHz 4800
+	BNE PWM_DONE  
+
+	; 斩波判断
+    LDR R1,=GPIOD   ; 取比较器状态
+    LDR R0,[R1]
+	ORR R6,R0
+	MOV R2,R5
+	EOR R2,R6	   ;
+	LDR R1,=GPIOB
+	STR R2,[R1]
+
+;PWM_INIT
+	MOV R7, #2400 ; 10KHz 2400
 	MOV R6,#0
 	LDR R1,=GPIOB
 	STR R5,[R1]
 PWM_DONE
-	B GOON
-
-	; 自动半流判断
-	; R11 自动半流 计数
-	SUBS R11,R11,#1
-	BNE GOON
-    LDR R1,=GPIOD
-	MOV R0,#0X128
-	STR R0,[R1]
-    B GOON
+	B GOON	
 
 
 
@@ -371,129 +347,7 @@ SysTick_Handler PROC
                 B       .
                 ENDP
 ;中断
-Default_Handler PROC
-
-                EXPORT  WWDG_IRQHandler            [WEAK]
-                EXPORT  PVD_IRQHandler             [WEAK]
-                EXPORT  TAMPER_IRQHandler          [WEAK]
-                EXPORT  RTC_IRQHandler             [WEAK]
-                EXPORT  FLASH_IRQHandler           [WEAK]
-                EXPORT  RCC_IRQHandler             [WEAK]
-                EXPORT  EXTI0_IRQHandler           [WEAK]
-                EXPORT  EXTI1_IRQHandler           [WEAK]
-                EXPORT  EXTI2_IRQHandler           [WEAK]
-                EXPORT  EXTI3_IRQHandler           [WEAK]
-                EXPORT  EXTI4_IRQHandler           [WEAK]
-                EXPORT  DMA1_Channel1_IRQHandler   [WEAK]
-                EXPORT  DMA1_Channel2_IRQHandler   [WEAK]
-                EXPORT  DMA1_Channel3_IRQHandler   [WEAK]
-                EXPORT  DMA1_Channel4_IRQHandler   [WEAK]
-                EXPORT  DMA1_Channel5_IRQHandler   [WEAK]
-                EXPORT  DMA1_Channel6_IRQHandler   [WEAK]
-                EXPORT  DMA1_Channel7_IRQHandler   [WEAK]
-                EXPORT  ADC1_2_IRQHandler          [WEAK]
-                EXPORT  USB_HP_CAN1_TX_IRQHandler  [WEAK]
-                EXPORT  USB_LP_CAN1_RX0_IRQHandler [WEAK]
-                EXPORT  CAN1_RX1_IRQHandler        [WEAK]
-                EXPORT  CAN1_SCE_IRQHandler        [WEAK]
-                EXPORT  EXTI9_5_IRQHandler         [WEAK]
-                EXPORT  TIM1_BRK_IRQHandler        [WEAK]
-                EXPORT  TIM1_UP_IRQHandler         [WEAK]
-                EXPORT  TIM1_TRG_COM_IRQHandler    [WEAK]
-                EXPORT  TIM1_CC_IRQHandler         [WEAK]
-                EXPORT  TIM2_IRQHandler            [WEAK]
-                EXPORT  TIM3_IRQHandler            [WEAK]
-                EXPORT  TIM4_IRQHandler            [WEAK]
-                EXPORT  I2C1_EV_IRQHandler         [WEAK]
-                EXPORT  I2C1_ER_IRQHandler         [WEAK]
-                EXPORT  I2C2_EV_IRQHandler         [WEAK]
-                EXPORT  I2C2_ER_IRQHandler         [WEAK]
-                EXPORT  SPI1_IRQHandler            [WEAK]
-                EXPORT  SPI2_IRQHandler            [WEAK]
-                EXPORT  USART1_IRQHandler          [WEAK]
-                EXPORT  USART2_IRQHandler          [WEAK]
-                EXPORT  USART3_IRQHandler          [WEAK]
-                EXPORT  EXTI15_10_IRQHandler       [WEAK]
-                EXPORT  RTCAlarm_IRQHandler        [WEAK]
-                EXPORT  USBWakeUp_IRQHandler       [WEAK]
-                EXPORT  TIM8_BRK_IRQHandler        [WEAK]
-                EXPORT  TIM8_UP_IRQHandler         [WEAK]
-                EXPORT  TIM8_TRG_COM_IRQHandler    [WEAK]
-                EXPORT  TIM8_CC_IRQHandler         [WEAK]
-                EXPORT  ADC3_IRQHandler            [WEAK]
-                EXPORT  FSMC_IRQHandler            [WEAK]
-                EXPORT  SDIO_IRQHandler            [WEAK]
-                EXPORT  TIM5_IRQHandler            [WEAK]
-                EXPORT  SPI3_IRQHandler            [WEAK]
-                EXPORT  UART4_IRQHandler           [WEAK]
-                EXPORT  UART5_IRQHandler           [WEAK]
-                EXPORT  TIM6_IRQHandler            [WEAK]
-                EXPORT  TIM7_IRQHandler            [WEAK]
-                EXPORT  DMA2_Channel1_IRQHandler   [WEAK]
-                EXPORT  DMA2_Channel2_IRQHandler   [WEAK]
-                EXPORT  DMA2_Channel3_IRQHandler   [WEAK]
-                EXPORT  DMA2_Channel4_5_IRQHandler [WEAK]
-
-				WWDG_IRQHandler
-				PVD_IRQHandler
-				TAMPER_IRQHandler
-				RTC_IRQHandler
-				FLASH_IRQHandler
-				RCC_IRQHandler
-				EXTI0_IRQHandler
-				EXTI1_IRQHandler
-				EXTI2_IRQHandler
-				EXTI3_IRQHandler
-				EXTI4_IRQHandler
-				DMA1_Channel1_IRQHandler
-				DMA1_Channel2_IRQHandler
-				DMA1_Channel3_IRQHandler
-				DMA1_Channel4_IRQHandler
-				DMA1_Channel5_IRQHandler
-				DMA1_Channel6_IRQHandler
-				DMA1_Channel7_IRQHandler
-				ADC1_2_IRQHandler
-				USB_HP_CAN1_TX_IRQHandler
-				USB_LP_CAN1_RX0_IRQHandler
-				CAN1_RX1_IRQHandler
-				CAN1_SCE_IRQHandler
-				EXTI9_5_IRQHandler
-				TIM1_BRK_IRQHandler
-				TIM1_UP_IRQHandler
-				TIM1_TRG_COM_IRQHandler
-				TIM1_CC_IRQHandler
-				TIM2_IRQHandler
-				TIM3_IRQHandler
-				TIM4_IRQHandler
-				I2C1_EV_IRQHandler
-				I2C1_ER_IRQHandler
-				I2C2_EV_IRQHandler
-				I2C2_ER_IRQHandler
-				SPI1_IRQHandler
-				SPI2_IRQHandler
-				USART1_IRQHandler
-				USART2_IRQHandler
-				USART3_IRQHandler
-				EXTI15_10_IRQHandler
-				RTCAlarm_IRQHandler
-				USBWakeUp_IRQHandler
-				TIM8_BRK_IRQHandler
-				TIM8_UP_IRQHandler
-				TIM8_TRG_COM_IRQHandler
-				TIM8_CC_IRQHandler
-				ADC3_IRQHandler
-				FSMC_IRQHandler
-				SDIO_IRQHandler
-				TIM5_IRQHandler
-				SPI3_IRQHandler
-				UART4_IRQHandler
-				UART5_IRQHandler
-				TIM6_IRQHandler
-				TIM7_IRQHandler
-				DMA2_Channel1_IRQHandler
-				DMA2_Channel2_IRQHandler
-				DMA2_Channel3_IRQHandler
-				DMA2_Channel4_5_IRQHandler
+Default_Handler PROC		 
                 B       .
 
                 ENDP
